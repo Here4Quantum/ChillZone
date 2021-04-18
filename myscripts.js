890064bc43a66a05d7705ccf4ee88bd47859f462
Here4Quantum <79421530+Here4Quantum@users.noreply.github.com>
2021-04-18 12:49:10 +0100

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function changesad(){
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/3gTmgzy7wYJfyaGRHQ/giphy.gif')";
+}
+
+function changehappy(){
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/13ZzYXkeIjcZy0/giphy.gif')";
+}
+
+function changeangry(){
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/IuVFGSQZTd6TK/giphy.gif')";
+}
+
+function changeneutral(){
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/BHNfhgU63qrks/giphy.gif')";
+}
