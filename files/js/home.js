cde93ec09e08a5f20fcf14de380e050fdf2e769e
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-22 15:39:41 -0700

    feat: added hourglass form

--- files/js/home.js
+++ files/js/home.js
@@ -1,3 +1,19 @@
 $('#angry').click(function(){
-    window.location = ""
+    window.location = "files/angry.html";
+})
+
+$('#sad').click(function(){
+    window.location = "files/sad.html";
+})
+
+$('#neutral').click(function(){
+    window.location = "files/neutral.html";
+})
+
+$('#happy').click(function(){
+    window.location = "files/happy.html";
+})
+
+$('#form').click(function(){
+    window.location = "files/form.html";
 })
