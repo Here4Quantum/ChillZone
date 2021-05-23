1cb5fc046ba139c57d0dc3527afa291160796209
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-22 17:50:01 -0700

    fix: form section

---
+++ files/js/form.js
@@ -0,0 +1,41 @@
+$("#name-invalid-feedback").css("display", "none");
+var formData = {
+    name: "",
+    email: "",
+    startWork: "",
+    endWork: "",
+    startSleep: "",
+    amountSleep: "",
+    amountFreetime: "",
+    amountRelationshipTime: "",
+    startFreetime: "",
+    breakfastTime: "",
+    lunchTime: "",
+    dinnerTime: "",
+    amountHobby: "",
+    amountHobby: ""
+}
+
+
+function holdFormData(){
+    formData.name = $("#name").val();
+    formData.email = $("#email").val();
+    formData.startWork = $("#startWork").val();
+    formData.endWork = $("#endWork").val();
+    formData.startSleep = $("#startSleep").val();
+    formData.amountSleep = $("#amountSleep").val();
+    formData.amountFreetime = $("#startFreetime").val();
+    formData.amountRelationshipTime = $("#amountRelationshipTime").val();
+    formData.startFreetime = $("#startFreetime").val();
+    formData.breakfastTime = $("#breakfastTime").val();
+    formData.lunchTime = $("#lunchTime").val();
+    formData.dinnerTime = $("#dinnerTime").val();
+    formData.amountHobby = $("#amountHobby").val();
+    formData.amountHobby = $("#amountHobby").val();
+}
+
+function postFormData(){
+    holdFormData();
+    localStorage.setItem("formData", JSON.stringify(formData));
+    window.location.href = window.location.origin + "/schedule";
+}
